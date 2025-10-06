6e1f0b145a72ccad8b8aa19e7b20431ea88c1462
DiriARG <matidirisio@gmail.com>
2025-10-06 19:45:11 -0300

    Ejercicio 3.20* resuelto.

--- models/persona.js
+++ models/persona.js
@@ -12,16 +12,29 @@ mongoose
     console.log("Conectado a MongoDB");
   })
   .catch((error) => {
-    console.log("Error al conectar a MongoDB:", error.message);
+    console.log("Error al conectar a MongoDB: ", error.message);
   });
 
 const personaSchema = new mongoose.Schema({
   name: {
     type: String,
     // Se agrega una restricción mínima de tres caracteres de longitud para el nombre.
-    minlength: 3,
+    minlength: [3, "El nombre debe tener al menos 3 caracteres de longitud."],
+    required: [true, "El nombre es obligatorio."],
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: [8, "El número debe tener al menos 8 caracteres de longitud."],
+    required: [true, "El número es obligatorio."],
+    validate: {
+      validator: function (valor) {
+        // Para tener el formato "xx-xxxxxxx" o "xxx-xxxxxxxx".
+        return /^\d{2,3}-\d+$/.test(valor);
+      },
+      message:
+        "El número debe tener el formato correcto (ej: 09-1234567 o 040-22334455).",
+    },
+  },
 });
 
 // Configura la opción "toJSON" del esquema.
